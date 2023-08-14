417b5b9510b9f103cd7240ed292a635d66a05136
Daniel <dannyking07@outlook.es>
2023-08-14 09:52:14 -0500

    feat [src/database -> database.js]

--- src/database/database.js
+++ src/database/database.js
@@ -1,1 +1,18 @@
-//Aqui se maneja la configuracion de la base de datos
+import mongoose from "mongoose";
+
+// setting environment variables -- database connection link
+import { config } from "dotenv";
+config();
+const MONGODB_URI = process.env.MONGODB_URI
+
+const connectionOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
+mongoose.set("strictQuery", false);
+
+// database connection
+mongoose
+  .connect(MONGODB_URI, connectionOptions )
+  .then((db) => console.log("Db is connected"))
+  .catch((error) => console.log(error));
