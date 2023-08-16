195338047e64efccb43928d88c2bf78cb39b02f2
santiago <santiagobenitez467@gmail.com>
2023-08-16 14:59:06 -0500

    feat:modified: src/controller/loginRegisterController.js

--- src/controller/loginRegisterController.js
+++ src/controller/loginRegisterController.js
@@ -1,1 +1,43 @@
+const loginRegisterController = {};
+
+//página de inicio de sesión
+loginRegisterController.showLogin = (req, res) => {
+  res.render('login');
+};
+
+//inicio de sesión
+loginRegisterController.processLogin = (req, res) => {
+  const { username, password } = req.body;
+
+  //autenticación
+  if (username === 'usuario' && password === 'contraseña') {
+    // autenticación exitosa
+    res.redirect('/dashboard');
+  } else {
+    // autenticación fallida (si los datos que ingresó el usuario estan mal)
+    res.render('login', { error: 'Credenciales inválidas' });
+  }
+};
+
+//página de registro
+loginRegisterController.showRegister = (req, res) => {
+  res.render('register');
+};
+
+// Procesar el registro
+loginRegisterController.processRegister = (req, res) => {
+  const { username, email, password } = req.body;
+
+  // registro
+  if (username && email && password) {
+    // Registro exitoso
+    res.redirect('/login');
+  } else {
+    // Error en el registro
+    res.render('register', { error: 'Por favor completa todos los campos' });
+  }
+};
+
+export default loginRegisterController;
+
 
