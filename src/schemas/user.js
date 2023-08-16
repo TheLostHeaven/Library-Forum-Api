a6cce148e2c942e98805be58f531f4f06e55dd72
Victor <vimatoca050106@gmail.com>
2023-08-15 22:55:34 -0500

    feat [schemas -> user.js]

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,4 +1,52 @@
-//Aqui se maneja los modelos de la app
+import {Schema, model} from "mongoose";
+import bcrypt from "bcryptjs";
 
+const userSchema = new Schema(
+  {
+    firstNames: {
+      type: String,
+      required : true,
+      
+    },
+    lastNames: {
+      type:String,
+      required: true,
+    },
+    email: {
+      type:String,
+      unique: true,
+      required: true,
+    },
+    phone: {
+      type: Number,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    enabled: {
+      type: Boolean,
+      default: true,
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
+userSchema.statics.compareRole = async (role, receiveRol) => {
+  return await (role == receiveRol);
+};
+
+userSchema.statics.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+userSchema.statics.comparePassword = async (password, receivePassword) => {
+  return await bcrypt.compare(password, receivePassword);
+};
+
+export default model("User", userSchema);
 
