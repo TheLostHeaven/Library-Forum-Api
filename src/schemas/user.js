46f644166a30d21800b5f95349e55fbc01a6c784
santiago <santiagobenitez467@gmail.com>
2023-08-16 15:02:48 -0500

    feat:modified: src/schemas/user.js

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,12 +1,1 @@
-import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  // Otros opciones para el usuario
-});
-
-const User = mongoose.model('user', userSchema);
-
-export default User;
