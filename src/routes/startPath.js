2aaa9d5d511db44fe3276c6cae8633803b489343
santiago <santiagobenitez467@gmail.com>
2023-08-17 09:46:14 -0500

    feat:deleted:src/routes/startPath.js

--- src/routes/startPath.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-//para mostrar los foros en la página de inicio
-router.get('/inicio', (req, res) => {
-  // Lógica para obtener y mostrar los foros
-});
-
-export default router;
