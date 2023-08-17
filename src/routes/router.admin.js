d27c65d10c4e5f6fbd17a28e70a87dd71e6c464c
santiago <santiagobenitez467@gmail.com>
2023-08-17 09:36:22 -0500

    feat:modified: src/routes/router.admin.js

---
+++ src/routes/router.admin.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+const router = express.Router();
+// para iniciar sesión como administrador
+router.post('/admin/login', async (req, res) => {
+  // Lógica de autenticación de administrador aquí
+});
+
+// para eliminar una publicación de un usuario (función de administrador)
+router.delete('/admin/posts/:postId', async (req, res) => {
+  // Lógica para eliminar la publicación con el ID especificado
+});
+
+export default router;
