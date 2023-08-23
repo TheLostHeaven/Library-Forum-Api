e2a1cf24d7517502eeee9e9f28ea08f20058f788
Daniel Molina <90277397+TheLostHeaven@users.noreply.github.com>
2023-08-23 12:15:22 -0500

    feat [src/route -> index.js]

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,11 @@
 import {Router} from 'express'
+import express from 'express'
 import pkg from "../../package.json" assert {type: "json"};
 
 const router = Router()
+const app = express();
+
+app.use(express.json());
 
 router.get("/", (req, res) => {
   res.json({
